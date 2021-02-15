d4f83c7ce3fd745d83da4eca45bec16a7642ebc3
Dan Sokolsky <dan.sokolsky@gmail.com>
2021-02-15 12:15:22 -0500

    Reformatting. Adding a comment

--- stochastic_analysis/homework_1/exponential_variable_mean_1.py
+++ stochastic_analysis/homework_1/exponential_variable_mean_1.py
@@ -1,20 +1,31 @@
 import numpy as np
 
+
 # A general exponential Random Variable has the pdf f(x) = λ * exp(-λ * x); with mean 1/λ.
 # An exponential Random Variable with mean 1, therefore, must satisfy λ = 1.
 # Note: l stands for λ (lambda), but this is a reserved python keyword.
 
+
 l = 1
-low=0
-high=10
+low = 0
+high = 10
+interval_length = high - low
+
 
 def get_weighted_sample():
     x = np.random.uniform(low, high)
     return x * l * np.exp(-l * x)
 
+
 def monte_carlo_exponential_variable_mean(num_random_vars, sim_runs=1000):
     averages = [np.average([get_weighted_sample() for _ in range(num_random_vars)]) for _ in range(sim_runs)]
-    return np.average(averages) * (high - low)
+
+    # a monte carlo simulation over $(0, 1)$ is the simple sum $1/N * \sum_{i=1}^{sim_runs} x * f_X(x)$
+    # for $x$ drawn uniformly from $(0, 1)$. If we want to simulate over $(0, interval_length)$,
+    # we must multiply by the length of of the interval to account for the factor in
+    # the change of variables $x ↦ interval_length * x$
+    return np.average(averages) * interval_length
+
 
 def main():
     print(
