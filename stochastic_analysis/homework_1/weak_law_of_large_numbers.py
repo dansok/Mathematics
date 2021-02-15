d08b3d5b511dfbe74774c312ae6901e0ef08d6f8
Dan Sokolsky <dan.sokolsky@gmail.com>
2021-02-15 16:01:20 -0500

    Fixing up homework_1/weak_law_of_large_numbers.py. There's a regression. Look into it

--- stochastic_analysis/homework_1/weak_law_of_large_numbers.py
+++ stochastic_analysis/homework_1/weak_law_of_large_numbers.py
@@ -7,11 +7,11 @@ def flip_coin():
 
 
 def main():
-    num_flips = 10000
+    num_flips = 100
     epsilon = 0.1
     bound = 1 / (4 * (epsilon ** 2))
     mu = 0.5
-    sim_runs = 100
+    sim_runs = 1000
     successes = np.array([[0] * sim_runs] * num_flips)
     upper_bounds = [bound / i for i in range(1, num_flips + 1)]
 
@@ -22,15 +22,18 @@ def main():
             # we may compute the averages vector using dynamic programming
             average = (i * last_average + x) / (i + 1)
             last_average = average
-            successes[i][j] = 0. if np.abs(average - mu) > epsilon else 1.
+            successes[i][j] = 1. if np.abs(average - mu) > epsilon else 0.
 
     probabilities = successes.sum(axis=0) / num_flips
+    m, n = successes.shape
+    print(f'm, n: {m, n}')
+    print(f'probabilities: {successes[5000:5005, 50:55]}')
     plt.xlabel('Number of flips')
     plt.ylabel('Probability of error deviating by a quantity greater than epsilon')
     plt.plot(probabilities, color='b', linestyle='dotted')
     # theoretical upper bounds
     plt.plot(upper_bounds, color='r', linestyle='dotted')
-    plt.show()
+    # plt.show()
 
 
 if __name__ == '__main__':
