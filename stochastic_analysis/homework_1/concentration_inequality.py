2243a0675ec68ca2c27ca17d4d7cc063f56e7966
Boris <test2001@sharklasers.com>
2021-02-16 21:54:57 -0500

    .

--- stochastic_analysis/homework_1/concentration_inequality.py
+++ stochastic_analysis/homework_1/concentration_inequality.py
@@ -5,10 +5,8 @@ import numpy as np
 def flip_coin():
     return np.random.randint(0, 2)
 
-
-def main():
+def get_log_probs(epsilon):
     num_flips = 300
-    epsilon = 0.1
     mu = 0.5
     sim_runs = 1000
     deviations = np.array([0] * num_flips)
@@ -22,16 +20,19 @@ def main():
             last_average = average
             if np.abs(average - mu) > epsilon:
                 deviations[i] += 1.
-
-    plt.xlabel('Number of flips')
-    plt.ylabel('Probability of error deviating by a quantity greater than epsilon')
     probabilities = deviations / sim_runs
-    xaxis_vals = np.arange(num_flips, step=10)
-    yaxis_vals = [probabilities[i] for i in xaxis_vals]
-    plt.scatter(xaxis_vals, yaxis_vals, color='b', label='Actual results')
-    # theoretical upper bounds
-    upper_bounds = [-1 / i * np.log(probabilities[i]) for i in range(1, num_flips)]
-    plt.plot(upper_bounds, color='r', label='Theoretical upper bound')
+    log_probs = [-1 / i * np.log(probabilities[i]) for i in range(1, num_flips)]
+    return log_probs
+
+def main():
+    plt.xlabel('Number of flips')
+    plt.ylabel('Log probabilities scaled by 1 / number of flips')
+    log_probs = get_log_probs(0.1)
+    plt.plot(log_probs, label='Epsilon = 0.1')
+    log_probs1 = get_log_probs(0.05)
+    plt.plot(log_probs1, label='Epsilon = 0.05')
+    log_probs2 = get_log_probs(0.01)
+    plt.plot(log_probs2, label='Epsilon = 0.01')
     plt.legend()
     plt.show()
 
