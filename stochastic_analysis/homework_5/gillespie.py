2c6c40d48d2349ac22a481f24411bc0f317719da
Dan Sokolsky <dan.sokolsky@gmail.com>
2021-04-18 22:28:56 -0400

    reformattig

--- stochastic_analysis/homework_5/gillespie.py
+++ stochastic_analysis/homework_5/gillespie.py
@@ -7,14 +7,14 @@ t_max = 10
 
 def calc_intensities(X, k):
     # species G, M, P, D, B
-    lmbd0 = k[0] * X[0]                # G -> G + M
-    lmbd1 = k[1] * X[1]                # M -> M + P
-    lmbd2 = k[2] * X[1]                # M -> {}
-    lmbd3 = k[3] * X[2]                # P -> {}
-    lmbd4 = k[4] * X[2] * (X[2] - 1)   # 2P -> D
-    lmbd5 = k[5] * X[3]                # D -> {}
-    lmbd6 = k[6] * X[0] * X[3]         # G + D -> B
-    lmbd7 = k[7] * X[4]                # B -> G + D
+    lmbd0 = k[0] * X[0]  # G -> G + M
+    lmbd1 = k[1] * X[1]  # M -> M + P
+    lmbd2 = k[2] * X[1]  # M -> {}
+    lmbd3 = k[3] * X[2]  # P -> {}
+    lmbd4 = k[4] * X[2] * (X[2] - 1)  # 2P -> D
+    lmbd5 = k[5] * X[3]  # D -> {}
+    lmbd6 = k[6] * X[0] * X[3]  # G + D -> B
+    lmbd7 = k[7] * X[4]  # B -> G + D
     return [lmbd0, lmbd1, lmbd2, lmbd3, lmbd4, lmbd5, lmbd6, lmbd7]
 
 
@@ -33,7 +33,7 @@ def gillespie(initial_x, k):
     X = np.array(initial_x)
     disp = np.array(displacements)
     t = 0
-    x_vals= []
+    x_vals = []
     x_ticks = [0]
     for value in X:
         x_vals.append([value])
@@ -59,14 +59,14 @@ def gillespie(initial_x, k):
 
 def set_displacements():
     # species G, M, P, D, B
-    displacements.append([0, 1, 0, 0, 0])    # G -> G + M
-    displacements.append([0, 0, 1, 0, 0])    # M -> M + P
-    displacements.append([0, -1, 0, 0, 0])   # M -> {}
-    displacements.append([0, 0, -1, 0, 0])   # P -> {}
-    displacements.append([0, 0, -2, 1, 0])   # 2P -> D
-    displacements.append([0, 0, 0, -1, 0])   # D -> {}
+    displacements.append([0, 1, 0, 0, 0])  # G -> G + M
+    displacements.append([0, 0, 1, 0, 0])  # M -> M + P
+    displacements.append([0, -1, 0, 0, 0])  # M -> {}
+    displacements.append([0, 0, -1, 0, 0])  # P -> {}
+    displacements.append([0, 0, -2, 1, 0])  # 2P -> D
+    displacements.append([0, 0, 0, -1, 0])  # D -> {}
     displacements.append([-1, 0, 0, -1, 1])  # G + D -> B
-    displacements.append([1, 0, 0, 1, -1])   # B -> G + D
+    displacements.append([1, 0, 0, 1, -1])  # B -> G + D
 
 
 def main():
